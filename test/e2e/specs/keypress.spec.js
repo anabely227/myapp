2082b4dc52dcdd67194f085a1126f303d6392d87
Anabely Núñez <anabely227@gmail.com>
2021-08-03 19:14:23 +0100

    Commit message

--- test/e2e/specs/keypress.spec.js
+++ test/e2e/specs/keypress.spec.js
@@ -4,6 +4,7 @@ module.exports = {
     // default: http://localhost:8080
     // see nightwatch.conf.js
     //const devServer = browser.globals.devServerURL
+    //Example testing
 
     let formy = browser.page.formy()
 
