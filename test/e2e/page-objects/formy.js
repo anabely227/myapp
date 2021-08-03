4fa22c924831bf104bf41af557d53f0a502f2d0b
Anabely Núñez <anabely227@gmail.com>
2021-08-03 18:59:45 +0100

    Commit message

---
+++ test/e2e/page-objects/formy.js
@@ -0,0 +1,11 @@
+module.exports = {
+  url: 'https://formy-project.herokuapp.com/keypress',
+  elements: {
+      inputName: {
+          selector: 'input[type=text]'
+      },
+      submit: {
+          selector: 'button[type=button]'
+      }
+  }
+}
---
+++ test/e2e/specs/keypress.spec.js
@@ -0,0 +1,16 @@
+module.exports = {
+  'formy keypress test': function (browser) {
+    // automatically uses dev Server port from /config.index.js
+    // default: http://localhost:8080
+    // see nightwatch.conf.js
+    //const devServer = browser.globals.devServerURL
+
+    let formy = browser.page.formy()
+
+    formy.navigate()
+      .assert.title('Formy')
+      .assert.visible('@inputName')
+      .setValue('@inputName','nightwatch')
+      .click('@submit')
+  }
+}
